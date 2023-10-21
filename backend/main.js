8491dcc170dead78a16d31a8781e7e76cc90bc3c
sqdshguy <drgerasimova8@gmail.com>
2023-10-21 21:49:16 +0300

    linting

--- backend/main.js
+++ backend/main.js
@@ -1,25 +1,22 @@
-const express = require("express");
-const whois = require("whois-json");
-const cors = require("cors");
+const express = require('express');
+const whois = require('whois-json');
+const cors = require('cors');
 
-const app = express(),
-    port = 3000;
+const app = express();
+const port = 3000;
 
 app.use(express.json());
 app.use(cors());
 
-app.get("/whois/:domain", (req, res) => {
-    const domain = req.params.domain;
-    whois(domain.replace("www.", ""))
-        .then((data) => {
-            res.json({ domain, data });
-        })
-        .catch((err) => {
-            console.log(err);
-            res.json({ domain, data: null });
-        });
+app.get('/whois/:domain', (req, res) => {
+  const { domain } = req.params;
+  whois(domain.replace('www.', ''))
+    .then((data) => {
+      res.json({ domain, data });
+    })
+    .catch((err) => {
+      res.json({ domain, data: null, error: err });
+    });
 });
 
-app.listen(port, () =>
-    console.log(`Server is running on http://localhost:${port}`)
-);
+app.listen(port, () => process.stdout.write(`Whois query server is running on http://localhost:${port}\n`));
--- popup.js
+++ popup.js
@@ -1,81 +1,77 @@
-const paymentDomains = ["olx.ua", "en.olx.ua", "something.olx.ua"];
+const paymentDomains = ['olx.ua', 'en.olx.ua', 'something.olx.ua'];
 
 function levenshteinDistance(str1, str2) {
-    const m = str1.length,
-        n = str2.length,
-        dp = new Array(m + 1).fill(0).map(() => new Array(n + 1).fill(0));
+  const m = str1.length;
+  const n = str2.length;
+  const dp = new Array(m + 1).fill(0).map(() => new Array(n + 1).fill(0));
 
-    for (let i = 0; i <= m; i++) {
-        for (let j = 0; j <= n; j++) {
-            if (i === 0) dp[i][j] = j;
-            else if (j === 0) dp[i][j] = i;
-            else if (str1[i - 1] === str2[j - 1]) dp[i][j] = dp[i - 1][j - 1];
-            else
-                dp[i][j] =
-                    1 + Math.min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]);
-        }
+  for (let i = 0; i <= m; i += 1) {
+    for (let j = 0; j <= n; j += 1) {
+      if (i === 0) dp[i][j] = j;
+      else if (j === 0) dp[i][j] = i;
+      else if (str1[i - 1] === str2[j - 1]) dp[i][j] = dp[i - 1][j - 1];
+      else dp[i][j] = 1 + Math.min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]);
     }
+  }
 
-    return dp[m][n];
+  return dp[m][n];
 }
 
 function similarityCheck(url) {
-    let domain = new URL(url).hostname;
-    for (const paymentDomain of paymentDomains) {
-        const distance = levenshteinDistance(domain, paymentDomain);
-        if (distance <= 2 && distance != 0) return false;
-    }
-    return true;
+  const domain = new URL(url).hostname;
+  for (let i = 0; i < paymentDomains.length; i += 1) {
+    const distance = levenshteinDistance(domain, paymentDomains[i]);
+    if (distance <= 2 && distance !== 0) return false;
+  }
+  return true;
 }
 
 function lazySSLverification(url) {
-    return url.startsWith("https://");
+  return url.startsWith('https://');
 }
 
 function suspiciousSymbolsCheck(url) {
-    const regex = /[^a-zA-Z0-9.:/-]/;
-    return url.length > 200 || regex.test(url);
-}
-
-async function getCurrentUrl() {
-    const tabs = await chrome.tabs.query({
-        active: true,
-        lastFocusedWindow: true,
-    });
-    return checkUrl(tabs[0].url);
+  const regex = /[^a-zA-Z0-9.:/-]/;
+  return url.length > 200 || regex.test(url);
 }
 
 function getWhoisData(url) {
-    const domain = new URL(url).hostname;
-    fetch(`http://localhost:3000/whois/${domain}`)
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error("Network response was not ok");
-            }
-            return response.json();
-        })
-        .then((data) => {
-            console.log("WHOIS data:", data);
-            // Do something with the WHOIS data here
-        })
-        .catch((error) => {
-            console.error("Error:", error);
-            // Handle errors here
-        });
+  const domain = new URL(url).hostname;
+  fetch(`http://localhost:3000/whois/${domain}`)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log('WHOIS data:', data);
+      // Do something with the WHOIS data here
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+      // Handle errors here
+    });
 }
 
 function checkUrl(url) {
-    const result = similarityCheck(url);
-    if (result) document.getElementById("result").textContent = "Safe";
-    else document.getElementById("result").textContent = "Fishing alert";
-    console.log(lazySSLverification(url));
-    console.log(suspiciousSymbolsCheck(url));
-    console.log(getWhoisData(url));
+  const result = similarityCheck(url);
+  if (result) document.getElementById('result').textContent = 'Safe';
+  else document.getElementById('result').textContent = 'Fishing alert';
+  console.log(lazySSLverification(url));
+  console.log(suspiciousSymbolsCheck(url));
+  console.log(getWhoisData(url));
+}
+
+async function getCurrentUrl() {
+  const tabs = await chrome.tabs.query({
+    active: true,
+    lastFocusedWindow: true,
+  });
+  return checkUrl(tabs[0].url);
 }
 
 checkUrl(getCurrentUrl());
 document
-    .getElementById("checkPage")
-    .addEventListener("click", () =>
-        checkUrl(document.getElementById("url").value)
-    );
+  .getElementById('checkPage')
+  .addEventListener('click', () => checkUrl(document.getElementById('url').value));
